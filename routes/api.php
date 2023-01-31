24090a1b4edddf70caa2ee74133e24b28d52a81c
PaulinFiori <paulofiori34@gmail.com>
2023-01-31 20:15:52 -0300

    adicionando jwt

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +19,18 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('carro', App\Http\Controllers\CarroController::class);
-Route::apiResource('cliente', App\Http\Controllers\ClienteController::class);
-Route::apiResource('locacao', App\Http\Controllers\LocacaoController::class);
-Route::apiResource('marca', App\Http\Controllers\MarcaController::class);
-Route::apiResource('modelo', App\Http\Controllers\ModeloController::class);
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+
+Route::group([
+    'middleware' => 'jwt.auth',
+], function ($router) {
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+    Route::apiResource('carro', App\Http\Controllers\CarroController::class);
+    Route::apiResource('cliente', App\Http\Controllers\ClienteController::class);
+    Route::apiResource('locacao', App\Http\Controllers\LocacaoController::class);
+    Route::apiResource('marca', App\Http\Controllers\MarcaController::class);
+    Route::apiResource('modelo', App\Http\Controllers\ModeloController::class);
+});
